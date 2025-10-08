3cf7585a811bd5502a1a99489264e4ad111bacb9
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-10-08 12:59:14 +0300

    feat: Add core MultiversX integration utilities

---
+++ src/lib/multiversx/index.ts
@@ -0,0 +1,12 @@
+/**
+ * MultiversX Integration Core
+ * ProfesorXTrader v2.0 - Advanced Multi-Chain Trading Platform
+ */
+
+export * from './config'
+export * from './wallet'
+export * from './api'
+export * from './contracts'
+export * from './utils'
+export * from './types'
+export * from './hooks'
